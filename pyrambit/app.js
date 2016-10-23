4b8aaa447baae29ac21d010ccf4444698e17573c
btcflip <b.elm0p2@gmail.com>
2016-10-23 12:32:41 +0200

    faucet attempt2

--- pyrambit/app.js
+++ pyrambit/app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'Pyrambit',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LdKBAoUAAAAAOI4O57xyV3LPJ02sjvJxPOEL4_P',  // <----- EDIT ME!
+  recaptcha_sitekey: '6LdKBAoUAAAAAPmii6hkBFMX1eQZ87rqcldXaK2q',  // <----- EDIT ME!
   redirect_uri: 'https://bitsino.xyz/pyrambit',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
